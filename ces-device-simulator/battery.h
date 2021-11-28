708a315111926ab7fcf78039603c6ecea513347e
Hafsa <38572050+Hafsa-N@users.noreply.github.com>
2021-11-28 13:56:02 -0500

    comments in battery.h

--- ces-device-simulator/battery.h
+++ ces-device-simulator/battery.h
@@ -13,12 +13,13 @@ public:
 
     int getLevel() const;
     void setLevel(int value);
-    void batteryLow(int);
 
 public slots:
 
 signals:
     void lowBattery(int); //sends signal that battery is low (2 or 5)
+    //not really sure how you want to implement this but I made an initial battery implementation
+    // in mainwindow for testing purposes
 
 
 private:
